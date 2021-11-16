caea1180cf99706c7a8b778b9a776c67391fe94f
Patrick <kimanthip@gmail.com>
2021-11-16 09:55:49 +0300

    added ticket list page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
-import { AddTicket } from "./pages/new-ticket/AddTicket.page";
 import { DefaultLayout } from "./layout/DefaultLayout";
-// import { Dashboard } from "./pages/dashboard/Dashboard.page";
 
+// import { AddTicket } from "./pages/new-ticket/AddTicket.page";
+// import { Dashboard } from "./pages/dashboard/Dashboard.page";
 // import { Entry } from "./pages/entry/Entry.page";
+import { TicketLists } from "./pages/ticket-list/TicketLists.page";
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       {/* <Entry /> */}
       <DefaultLayout>
         {/* <Dashboard /> */}
-        <AddTicket />
+        {/* <AddTicket /> */}
+        <TicketLists />
       </DefaultLayout>
     </div>
   );
